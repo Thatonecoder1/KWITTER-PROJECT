242980164af9915e569a3752b9e75d5c18630608
Thatonecoder1 <83841772+Thatonecoder1@users.noreply.github.com>
2021-07-19 20:59:50 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,24 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+      apiKey: "AIzaSyA_ratyqbGvSrzfXROqO_FkkoUpB3TJdck",
+      authDomain: "project-42-444cb.firebaseapp.com",
+      databaseURL: "https://project-42-444cb.firebaseio.com",
+      projectId: "project-42-444cb",
+      storageBucket: "project-42-444cb.appspot.com",
+      messagingSenderId: "423675848777",
+      appId: "1:423675848777:web:9ec69e9694c6302635f798",
+      measurementId: "G-453WMDPLWX"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
